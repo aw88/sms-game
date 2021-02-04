4a06ffe03f4a49f74e13e546c1fc18754ca88ffc
Alex Walton <alex.w.walton@gmail.com>
2021-02-04 13:14:19 +0000

    Add asset build to Makefile

--- Makefile
+++ Makefile
@@ -1,19 +1,27 @@
 CC=sdcc
 
-REL_FILES=out/main.rel
+REL_FILES=out/main.rel out/bank2.rel
+
+BANKS=-Wl-b_BANK2=0x8000
 
 $(shell mkdir -p out/)
 
 game.sms: out/game.ihx
 	ihx2sms out/game.ihx game.sms
 
-out/game.ihx: $(REL_FILES)
+out/game.ihx: $(REL_FILES) assets
 	$(CC) -mz80 --no-std-crt0 --data-loc 0xC000 \
 		-o out/game.ihx \
+		$(BANKS) \
 		crt0/crt0_sms.rel $(REL_FILES) lib/SMSlib.lib
 
 out/main.rel: src/main.c
 	$(CC) -mz80 -c --std-sdcc99 src/main.c -o out/main.rel
+
+.PHONY: assets
+assets:
+	assets2banks ./assets --compile
+	mv bank*.h bank*.rel out/
 
 clean:
 	rm -rf out/
--- src/main.c
+++ src/main.c
@@ -1,13 +1,41 @@
 #include "SMSlib.h"
+#include "../out/bank2.h"
 
 #include "rom_header.h"
 
+#define TEXT_ASCII_OFFSET_MASK 0x80
+
+const unsigned char palette[] = {
+    0x00, 0x3f, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+};
+
+void write_string_at(char tile_x, char tile_y, char* string);
+
 void main(void) {
     SMS_init();
+
+    SMS_loadBGPalette(palette);
+    SMS_loadTiles(font, 0xA0, font_size);
+
+    write_string_at(4, 20, "Hello, world!");
 
     SMS_displayOn();
 
     for(;;) {
         SMS_waitForVBlank();
+    }
+}
+
+void write_string_at(char tile_x, char tile_y, char* string) {
+    SMS_setNextTileatXY(tile_x, tile_y);
+
+    char *c = string;
+
+    while (*c) {
+        int next_tile = (*c++) | TEXT_ASCII_OFFSET_MASK;
+        SMS_setTile(next_tile);
     }
 }
